8d3cdaa6372434dd0f2106bd8bc85fcb74d30c45
Nauman Ahmed <ahaqsan02@gmail.com>
2023-08-29 22:35:30 +0500

    episode08 diff bw class based comp and funct comp

--- src/components/User.js
+++ src/components/User.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function User(props) {
+  const [count, setCount] = useState(1);
+  const [count2, setCount2] = useState(2);
+  const { name, location } = props;
   return (
     <div className="user-card">
-        <h2>Name : {props.name}</h2>
-        <h2>This i s Namaste React WebSeries</h2>
+      <h2>Count : {count}</h2>
+      <h2>Count2 : {count2}</h2>
+      <h2>Name : {name}</h2>
+      <h2>Location : {location}</h2>
+      <h2>This i s Namaste React WebSeries</h2>
     </div>
-  )
+  );
 }
 
-export default User
+export default User;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,17 +1,26 @@
 import React from "react";
- 
-class UserClass extends React.Component{
-    constructor(props){
+
+class UserClass extends React.Component {
+    constructor(props) {
         super(props);
         console.log(props)
+        this.state = {
+            count: 1,
+            count2: 2, //useState is doing this internally
+        }
     }
-    render(){
+    render() {
+        const { name, location } = this.props;
+        const { count, count2 } = this.state;
         return (
             <div className="user-card">
-                <h2>Name : {this.props.name}</h2>
+                <h2>Count : {count}</h2>
+                <h2>Count2 : {count2}</h2>
+                <h2>Name : {name}</h2>
+                <h2>Location : {location}</h2>
                 <h2>This i s Namaste React WebSeries</h2>
             </div>
-          );
+        );
     }
 }
 
