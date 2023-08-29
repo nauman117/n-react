224affafe6fa60795c1586924d3d23931562bb1b
Nauman Ahmed <ahaqsan02@gmail.com>
2023-08-29 22:47:57 +0500

    episode08p04 state & setStates in class base comp

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -9,13 +9,24 @@ class UserClass extends React.Component {
             count2: 2, //useState is doing this internally
         }
     }
-    render() {
+    render() { 
         const { name, location } = this.props;
         const { count, count2 } = this.state;
         return (
             <div className="user-card">
                 <h2>Count : {count}</h2>
                 <h2>Count2 : {count2}</h2>
+                <button onClick={()=>{
+                    //Never update stae variables directly
+                    this.setState(
+                        {
+                            count:this.state.count+1,
+                            count2:this.state.count2+2
+                        }
+                    )
+                }}
+                >
+                    Count Increase</button>
                 <h2>Name : {name}</h2>
                 <h2>Location : {location}</h2>
                 <h2>This i s Namaste React WebSeries</h2>
