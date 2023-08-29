94752c5552ecaf426487fe0fd4a26163bc955fda
Nauman Ahmed <ahaqsan02@gmail.com>
2023-08-30 00:27:40 +0500

    episode08 changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,39 @@
 import React from 'react'
+import UserClass from './UserClass';
+import User from './User';
 
-function About() {
-  return (
-    <div>
-        <h1>About</h1>
-        <h2>This is a tutorial.</h2>
+class About extends React.Component {
+  constructor(props){
+    super(props);
+
+    // console.log("parent constructor")
+  }
+
+  componentDidMount() {
+      // console.log("parent componentDidMount")
+  }
+
+  render(){
+    // console.log("parent render")
+
+    return (
+      <div>
+        <h1>Contact us Page</h1>
+        {/* <User name="Nauman (function)" location="Lahore"/> */}
+        <UserClass name="First (class)" location="Lahore"/>
+        {/* <UserClass name="Second (class)" location="Lahore"/> */}
     </div>
-  )
+    )
+  }
 }
+
+// function About() {
+//   return (
+//     <div>
+//         <h1>About</h1>
+//         <h2>This is a tutorial.</h2>
+//     </div>
+//   )
+// }
 
 export default About
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,39 +1,10 @@
 import React from 'react'
-import User from './User'
-import UserClass from "./UserClass"
-
-class Contact extends React.Component {
-  constructor(props){
-    super(props);
-
-    // console.log("parent constructor")
-  }
-
-  componentDidMount() {
-      // console.log("parent componentDidMount")
-  }
-
-  render(){
-    // console.log("parent render")
-
-    return (
-      <div>
+function Contact() {
+  return (
+    <div>
         <h1>Contact us Page</h1>
-        {/* <User name="Nauman (function)" location="Lahore"/> */}
-        <UserClass name="First (class)" location="Lahore"/>
-        {/* <UserClass name="Second (class)" location="Lahore"/> */}
     </div>
-    )
-  }
+  )
 }
-// function Contact() {
-//   return (
-//     <div>
-//         <h1>Contact us Page</h1>
-//         <User name="Nauman (function)" location="Lahore"/>
-//         <UserClass name="Nauman (class)" location="Lahore"/>
-//     </div>
-//   )
-// }
 
 export default Contact
